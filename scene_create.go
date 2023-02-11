751f87e8b65284974566344ddd8964241045ab5d
sidav <sidav94@gmail.com>
2023-02-12 00:40:53 +0300

    Adds more generation tries.

--- scene_create.go
+++ scene_create.go
@@ -4,33 +4,38 @@ import "tbs4x/lib/perlin_noise"
 
 func (s *scene) init(w, h int) {
 	s.currentTurn = 1
-	// Perlin noise generation
-	noiseMap := perlin_noise.GeneratePerlinNoiseMap(w, h, 16, 8, rnd)
-	perlin_noise.MakeNoiseRectangular(noiseMap)
-	s.tiles = make([][]*tile, len(noiseMap))
-	for i := range s.tiles {
-		s.tiles[i] = make([]*tile, len(noiseMap[i]))
-	}
-	for x := range noiseMap {
-		for y := range noiseMap[x] {
-			switch {
-			case noiseMap[x][y] < 0.5:
-				s.tiles[x][y] = &tile{code: TILE_WATER}
-			case noiseMap[x][y] < 0.575:
-				s.tiles[x][y] = &tile{code: TILE_SAND}
-			case noiseMap[x][y] < 0.90:
-				s.tiles[x][y] = &tile{code: TILE_GRASS}
-			default:
-				s.tiles[x][y] = &tile{code: TILE_MOUNTAIN}
+	for i := 0; i < 100; i++ {
+		// Perlin noise generation
+		noiseMap := perlin_noise.GeneratePerlinNoiseMap(w, h, 16, 8, rnd)
+		perlin_noise.MakeNoiseRectangular(noiseMap)
+		s.tiles = make([][]*tile, len(noiseMap))
+		for i := range s.tiles {
+			s.tiles[i] = make([]*tile, len(noiseMap[i]))
+		}
+		for x := range noiseMap {
+			for y := range noiseMap[x] {
+				switch {
+				case noiseMap[x][y] < 0.5:
+					s.tiles[x][y] = &tile{code: TILE_WATER}
+				case noiseMap[x][y] < 0.575:
+					s.tiles[x][y] = &tile{code: TILE_SAND}
+				case noiseMap[x][y] < 0.90:
+					s.tiles[x][y] = &tile{code: TILE_GRASS}
+				default:
+					s.tiles[x][y] = &tile{code: TILE_MOUNTAIN}
+				}
 			}
 		}
-	}
-	s.generateResources()
+		s.generateResources()
 
-	s.addPlayer()
+		if s.tryAddPlayer() {
+			return
+		}
+	}
+	panic("Player is unplaceable")
 }
 
-func (s *scene) addPlayer() {
+func (s *scene) tryAddPlayer() bool {
 	newPlayer := &player{}
 	newPlayer.init(s.getSize())
 	x, y := -1, -1
@@ -39,7 +44,7 @@ func (s *scene) addPlayer() {
 		x, y = s.getRandomCoords()
 		try++
 		if try > 10000 {
-			panic("Can't place player city")
+			return false
 		}
 	}
 	startCity := &city{
@@ -60,6 +65,7 @@ func (s *scene) addPlayer() {
 	startUnit.initByStatic()
 	s.addUnit(startUnit)
 	s.players = append(s.players, newPlayer)
+	return true
 }
 
 func (s *scene) generateResources() {
